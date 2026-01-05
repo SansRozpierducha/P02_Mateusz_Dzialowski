d00f329dbe24eb004bf3977ac999a34e54006c88
Mateusz Działowski <mateusz.dzialowski@icloud.com>
2026-01-05 18:09:05 +0100

    Ulepszenie Algorytmu z Maksymalną tabliczą o wartości 11, co pozwala na rozwiazanie zadania z jaką kolwiek tablica

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <random>
 
-// Funkcja działająca tylko na danym przykladzie {1, 130, 1, 9, 11, 6, 1, 1, 1, 3, 1}, prawdopodobnie (nie było testow);
 void podciag(int ciag_a[], int N) {
     if (N < 5) {
         std::cout << "Ciag jest za krotki.\n";
     }
     const int M = 5;
     // int wynik[M]; 
-    for(int i = 0; i <= N - 4; i++) {
+    for(int i = 0; i < N - 4; i++) {
         int Temp[M];
         for(int j = 0; j < M; j++) {
             Temp[j] = ciag_a[i + j];
@@ -22,7 +22,16 @@ void podciag(int ciag_a[], int N) {
 }
 int main () {
     const int N = 11;
-    int ciag_wejsciowy[N] = {1, 130, 1, 9, 11, 6, 1, 1, 1, 3, 1};
+    // int ciag_wejsciowy[N] = {1, 130, 1, 9, 11, 6, 1, 1, 1, 3, 1};
+    int ciag_wejsciowy[N] = {};
+    std::srand(std::time(NULL));
+    for (int i = 0; i < N-1; i++) {
+        ciag_wejsciowy[i] = rand() % 150 + 1; // Losowe liczby od 1 do 150
+    }
+    std::cout << "Ciag wejsciowy: ";
+    for (int i = 0; i < N; i++) {
+        std::cout << ciag_wejsciowy[i] << " ";
+    }
     podciag(ciag_wejsciowy, N);
     return 0;
 }
