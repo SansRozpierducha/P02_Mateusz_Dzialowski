25ad9d63ed23a20b2e3804ba5cf08645cb301b0d
Mateusz Działowski <mateusz.dzialowski@icloud.com>
2026-01-20 19:48:54 +0100

    rozpoczęcie pracy nad Sprawodzaniem

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,26 @@ struct Wynik {
 };
 
 int podciag(int ciag[], int N, int wynik[]) {
-    //program konczy sie, jesli tablica ciag jest mniejszy niz 5
+    //program kończy się, jeśli tablica ciąg jest mniejszy niż 5
     if (N < 5) {
         return -1;
     }
     //zmienne
     const int M = 5;
     int count{0};
-    //glowna petla algorytmu - przeszukiwanie wszystkich mozliwych podciagów
+    //główna pętla algorytmu - przeszukiwanie wszystkich możliwych podciągów
     for(int i = 0; i < N - 4; i++) {
-        //wewnetrzna petla algorytmu - szukanie warunku spelnienia
+        //wewnętrzna pętla algorytmu - szukanie spełnienia warunku
         for(int j = i; j < N - 4; j++) {
-            //petla iteracyjna po elementach podciagu
+            //pętla iteracyjna po elementach podciągu
             for(int k = 0; k < M; k++) {
                 //sprawdzenie warunku: suma elementów na pozycjach nieparzystych > suma pozostałych
                 if(ciag[i+1] + ciag[i+3] > ciag[i] + ciag[i+2] + ciag[i+4]) {
-                    //przepisanie znalezionego podciagu do tablicy wynikow
+                    //przepisanie znalezionego podciągu do tablicy wyników
                     for(int m = 0; m < M; m++) {
                         wynik[count * M + m] = ciag[i + m];
                     }
-                    //inkrementacja licznika znalezionych podciagów
+                    //inkrementacja licznika znalezionych podciągów
                     count++;
                     //przerwanie pętli wewnętrznych po znalezieniu podciągu
                     j = N;
@@ -39,40 +39,40 @@ int podciag(int ciag[], int N, int wynik[]) {
             }
         }
     }
-    //zwrócenie liczby znalezionych podciagów
+    //zwrócenie liczby znalezionych podciągów
     return count;
 }
 
 int podciag_wersja_2(int ciag[], int N, int wynik[]) {
-    //program konczy sie, jesli tablica ciag jest mniejszy niz 5
+    //program kończy się, jeśli tablica ciąg jest mniejszy niż 5
     if (N < 5) {
         return -1;
     }
     // zmienne
     const int M = 5;
     int Temp[M], count{0};
-    //glowna petla algorytmu - przeszukiwanie wszystkich mozliwych podciagów
+    //główna pętla algorytmu - przeszukiwanie wszystkich możliwych podciągów
     for(int i = 0; i < N - 4; i++) {
-        //petla wewnetrzna - przypisuje wartosci do tablicy Temp
+        //pętla wewnętrzna - przypisuje wartości do tablicy Temp
         for(int j = 0; j < M; j++) {
             Temp[j] = ciag[i + j];
         }
         //sprawdzenie warunku: suma elementów na pozycjach nieparzystych > suma pozostałych
         if(Temp[1] + Temp[3] > Temp[0] + Temp[2]+ Temp[4]) {
-            //petla wewnetrzna - przypisujaca z tablicy Temp wyniki do tablicy Wyniki
+            //pętla wewnętrzna - przypisująca z tablicy Temp wyniki do tablicy Wyniki
             for(int k = 0; k < M; k++) {
                 wynik[count * M + k] = Temp[k];
             }
-            //inkrementacja licznika znalezionych podciagów
+            //inkrementacja licznika znalezionych podciągów
             count++;
         }
     }
-    //zwrócenie liczby znalezionych podciagów
+    //zwrócenie liczby znalezionych podciągów
     return count; 
 }
 
 void generuj_dane(int ciag[], int N) {
-    //generuje dane po przez bibliteke random
+    //generuje dane poprzez bibliotekę random
     std::srand(std::time(NULL));
     for (int i = 0; i < N; i++) {
         ciag[i] = rand() % 150 + 1; // rand()  % (max - min + 1) + min
@@ -80,7 +80,7 @@ void generuj_dane(int ciag[], int N) {
 }
 
 void wyswietl_dane(int tablica[], int N) {
-    //wyswietla dane poczatkowe (wygenerowane)
+    //wyświetla dane początkowe (wygenerowane)
     std::cout << "Ciag wejsciowy: ";
     for (int i = 0; i < N; i++) {
         std::cout << tablica[i] << " ";
@@ -89,7 +89,7 @@ void wyswietl_dane(int tablica[], int N) {
 }
 
 void wyswietl_wynik(int wynik[], int count) {
-    //wyswietla podciagi ktore zostaly znalezione
+    //wyświetla podciągi które zostały znalezione
     if(count > 0) {
         int k{0};
         for (int i = 0; i < count; i++) {
@@ -104,18 +104,18 @@ void wyswietl_wynik(int wynik[], int count) {
         std::cout << std::endl;
     }
     else {
-        std::cout << "Ciag jest za krotki.\n";
+        std::cout << "Ciag jest za krotki lub nie ma zadnego spelnionego ciagu\n";
     }
 }
 
 Wynik test_zestawow(int *tab, int N, int *dane) {
-    //funkcja do niewygodnych testow
+    //funkcja do niewygodnych testów
     int wynik = podciag(tab, N, dane);
     int wynik2 = podciag_wersja_2(tab, N, dane);
     return {wynik, wynik2};
 }
 void test_niewygodnych_zestawow() {
-    //funkcja zwiazana z niewygodnymi testami
+    //funkcja związana z niewygodnymi testami
     int tab1[] = {1,3,2,3};
     int tab2[] = {7,7,7,7,7};
     int tab3[] = {1, 130, 1, 9, 11, 6, 1, 1, 1, 3, 1};
@@ -149,7 +149,7 @@ void test_wydajności() {
     double* czas1 = new double[liczba_testow];
     double* czas2 = new double[liczba_testow];
 
-    //Generowanie danych i wykonywanie testow
+    //Generowanie danych i wykonywanie testów
     for(int i = 0; i < liczba_testow; i++) {
         int* ciag_wejsciowy = new int[N[i]]();
         int* wynik = new int[N[i]*5]();
@@ -159,13 +159,13 @@ void test_wydajności() {
         //wyświetlanie danych 
         // wyswietl_dane(ciag_wejsciowy, N);
 
-        //Rozpoczęcie głownego algorytmu
+        //Rozpoczęcie głównego algorytmu
         clock_t start1 = clock();
         int count1 = podciag(ciag_wejsciowy, N[i], wynik);
         clock_t stop1 = clock();
         czas1[i] = (double)(stop1 - start1) / CLOCKS_PER_SEC;
 
-        //Rozpoczęcie głownego algorytmu drugiego
+        //Rozpoczęcie głównego algorytmu drugiego
         clock_t start2 = clock();
         int count2 = podciag_wersja_2(ciag_wejsciowy, N[i], wynik);
         clock_t stop2 = clock();
@@ -174,7 +174,7 @@ void test_wydajności() {
         delete[] ciag_wejsciowy;
         delete[] wynik;
     }
-    //Wyświetlanie w konsoli czasu algorytmow
+    //Wyświetlanie w konsoli czasu algorytmów
     std::cout << "L.p.  n   t1[s]    t2[s]" << "\n";
     for(int i = 0; i < liczba_testow; i++) {
         std::cout << std::fixed << std::setprecision(6) << i+1 << "   " << N[i] << "    " << czas1[i] << "    " << czas2[i] << '\n';
